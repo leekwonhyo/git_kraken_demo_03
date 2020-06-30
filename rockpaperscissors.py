e441a042b280e275a60b154c34d7f5e5b52a812f
leekwonhyo <kensi0512@gmail.com>
2020-06-30 21:45:53 +0900

    Add messages and winning rule

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,9 +3,27 @@ from random import randint
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
+# list of messages
+messages = ["Tie!", "You win!", "You lose!"]
+
+# game rule
+winning_rule = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
+
 # get the user input
 player = input("Rock", "Paper", "Scissors? ")
 
 # assign a random play to the computer
 computer = play[randint(0, 2)]
 print('Computer: {}'.format(computer))
+
+# ite
+if player == computer:
+    print(message[0])
+
+# you win
+elif winning_rule[player] == computer:
+    print(messages[1])
+
+# you lose
+else:
+    print(messages[2])
