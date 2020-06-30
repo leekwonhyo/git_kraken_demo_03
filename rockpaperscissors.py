fd31f734d824bf931b8d6333f43ee3d26ab59767
leekwonhyo <kensi0512@gmail.com>
2020-06-30 21:50:54 +0900

    Introduce an infinite loop
    Add the quit condition

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,6 +3,7 @@ from random import randint
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
+<<<<<<< master
 # list of messages
 messages = ["Tie!", "You win!", "You lose!"]
 
@@ -27,3 +28,47 @@ elif winning_rule[player] == computer:
 # you lose
 else:
     print(messages[2])
+=======
+while True:
+    # get the user input
+    player = input("Rock", "Paper", "Scissors? ")
+
+    # assign a random play to the computer
+    computer = play[randint(0, 2)]
+    print('Computer: {}'.format(computer))
+
+    # tie
+    if player == computer:
+        print("Tie!")
+    
+    # rock
+    elif player == "Rock":
+        if computer == "Scissors":
+            print("You win!")
+        else:
+            print("you lose!")
+    
+    # paper
+    elif player == "Paper":
+        if computer == "Rock":
+            print("You win!")
+        else:
+            print("you lose!")
+
+    # scissors
+    elif player == "Scissors":
+        if computer == "Paper":
+            print("You win!")
+        else:
+            print("you lose!")
+
+    # quit
+    else:
+        break
+    
+    # new line
+    print()
+
+# bye
+print("Bye!")
+>>>>>>> Introduce an infinite loop
