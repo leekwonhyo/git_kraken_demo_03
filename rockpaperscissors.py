907b6d287583593ce60c26dc67751e6a43bb3054
leekwonhyo <kensi0512@gmail.com>
2020-06-30 21:40:09 +0900

    Display both of the plays

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,6 +3,8 @@ play = ["Rock", "Paper", "Scissors"]
 
 # assign a random play to the computer
 computer = "Rock"
+print('Computer: {}'.format(computer))
 
 # get the user input
 player = "Paper"
+print('Player: {}'.format(player))
